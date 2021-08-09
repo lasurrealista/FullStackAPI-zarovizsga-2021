e2db0e695281af77f46891dd1b4d3ba99f5ebc73
Evelin <ladyblue@freemail.hu>
2021-08-09 14:30:10 +0200

    1st task

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,37 @@
 // 1. Feladat: pótold a service modellt.
+
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    street: String,
+    city: String,
+    country: String,
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('service', ServiceSchema);
+
+
+/*
+  _id: string = '';
+  name: string = '';
+  since: number = 0;
+  street?: string = '';
+  city?: string = '';
+  country?: string = '';
+  active?: boolean = true;
+*/
